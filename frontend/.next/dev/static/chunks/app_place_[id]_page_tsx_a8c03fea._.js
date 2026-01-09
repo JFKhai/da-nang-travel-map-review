12d4b59e3f26a5dee9cae8837842d9db63871971
Dung Văn <dungvan.170204@gmail.com>
2026-01-09 21:02:14 +0700

    fix: delete frontend folder

--- frontend/.next/dev/static/chunks/app_place_[id]_page_tsx_a8c03fea._.js
+++
@@ -1,8 +0,0 @@
-(globalThis.TURBOPACK_CHUNK_LISTS || (globalThis.TURBOPACK_CHUNK_LISTS = [])).push({
-    script: typeof document === "object" ? document.currentScript : undefined,
-    chunks: [
-  "static/chunks/node_modules_7e37a411._.js",
-  "static/chunks/app_a91a480c._.js"
-],
-    source: "dynamic"
-});

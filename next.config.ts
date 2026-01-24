e6cbe01daf541c27dcb7484a6f20f4035763b1aa
Khải Trần Quang <khaitran955@gmail.com>
2026-01-24 17:53:11 +0700

    fix: update next.config.ts to allow external image domains and SVG

--- next.config.ts
+++ next.config.ts
@@ -2,7 +2,23 @@ import type { NextConfig } from 'next'
 
 const nextConfig: NextConfig = {
   images: {
+    dangerouslyAllowSVG: true,
+    contentDispositionType: 'attachment',
+    contentSecurityPolicy: "default-src 'self'; script-src 'none'; sandbox;",
+
     remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'avatars.githubusercontent.com',
+        port: '',
+        pathname: '/**',
+      },
+      {
+        protocol: 'https',
+        hostname: 'cdn.jsdelivr.net',
+        port: '',
+        pathname: '/**',
+      },
       {
         protocol: 'https',
         hostname: 'images.unsplash.com',
